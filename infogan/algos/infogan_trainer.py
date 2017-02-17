ef9b4039a587ed6b3b3633dbb6910ca4edc75889
Cusuh <cusuhh@gmail.com>
2017-02-17 13:38:22 -0500

    Fixed deprecation warnings

--- infogan/algos/infogan_trainer.py
+++ infogan/algos/infogan_trainer.py
@@ -121,7 +121,7 @@ class InfoGANTrainer(object):
             self.generator_trainer = pt.apply_optimizer(generator_optimizer, losses=[generator_loss], var_list=g_vars)
 
             for k, v in self.log_vars:
-                tf.scalar_summary(k, v)
+                tf.summary.scalar(k, v)
 
         with pt.defaults_scope(phase=pt.Phase.test):
             with tf.variable_scope("model", reuse=True) as scope:
@@ -202,20 +202,20 @@ class InfoGANTrainer(object):
                 stacked_img.append(tf.concat(1, row_img))
             imgs = tf.concat(0, stacked_img)
             imgs = tf.expand_dims(imgs, 0)
-            tf.image_summary("image_%d_%s" % (dist_idx, dist.__class__.__name__), imgs)
+            tf.summary.image("image_%d_%s" % (dist_idx, dist.__class__.__name__), imgs)
 
 
     def train(self):
 
         self.init_opt()
 
-        init = tf.initialize_all_variables()
+        init = tf.global_variables_initializer()
 
         with tf.Session() as sess:
             sess.run(init)
 
-            summary_op = tf.merge_all_summaries()
-            summary_writer = tf.train.SummaryWriter(self.log_dir, sess.graph)
+            summary_op = tf.summary.merge_all()
+            summary_writer = tf.summary.FileWriter(self.log_dir, sess.graph)
 
             saver = tf.train.Saver()
 
